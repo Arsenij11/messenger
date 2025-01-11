b74598c8d275ba69fcc30a4dca06abc0d844cb5a
Arsenii Burkov <133581818+Arsenij11@users.noreply.github.com>
2025-01-11 07:03:11 +0300

    Delete messenger/users directory

--- messenger/users/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from chats.models import Account
-
-
-class AccountSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Account
-        fields = '__all__'
