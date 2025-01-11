48d97a4e3ff4faeccc93d817379aafce1a04f195
Arsenii Burkov <133581818+Arsenij11@users.noreply.github.com>
2025-01-11 07:02:02 +0300

    Delete messenger/chats directory

--- messenger/chats/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from chats.models import Account, Chat, Message
-
-# Register your models here.
-
-admin.site.register(Account)
-admin.site.register(Chat)
-admin.site.register(Message)
